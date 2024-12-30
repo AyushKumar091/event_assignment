7e0cf8d1c762f0524df7648c5630da63480b262e
Ayush Kumar <ayushkr09@gmail.com>
2024-12-30 16:17:37 +0530

    Backend Task done

--- ai_calc_backend-main/controllers/user.controller.js
+++ ai_calc_backend-main/controllers/user.controller.js
@@ -32,19 +32,15 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ message: 'Invalid email or password.' });
     }
-
     const isPasswordMatch = await bcrypt.compare(password, user.password);
     if (!isPasswordMatch) {
       return res.status(401).json({ message: 'Invalid email or password.' });
     }
-
     const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
-
     res.status(200).json({
       message: 'Login successful.',
       token,
